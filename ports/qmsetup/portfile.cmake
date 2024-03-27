1c2eafc9af8c14d3770d9a0a55bc974538fe1677
Sine Striker <trueful@163.com>
2024-03-27 15:11:39 +0800

    update

--- ports/qmsetup/portfile.cmake
+++ ports/qmsetup/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stdware/qmsetup
-    REF 4bdc73271a9e3b6d8cc0a0affda18ff008c901d2
-    SHA512 627bfa8b66a311e6ae4ed9d03c2ce67885495bc9ada7015d7a8cf14bfe4a524ebaf575bfbea51d14f245a6d6b897502ba8a3249d69db76a741b8537dbc4e6026
+    REF 72c92874ff977ec1fe6ad0630b125f13913fc04e
+    SHA512 80729b522df9d54d35d2e6f9f9f04dfb44105a107f92108824307aa52ceac69ecad0150fa55792e5d61759152d24aedceab3d2c1ed537241c5be350da12940e4
 )
 
 vcpkg_cmake_configure(
