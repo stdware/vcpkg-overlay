8a983b910c50b0fbdb17262661e39d0a70d2ad4a
Sine Striker <trueful@163.com>
2024-10-20 00:34:08 +0800

    Update syscmdline and qmsetup

--- ports/qmsetup/portfile.cmake
+++ ports/qmsetup/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stdware/qmsetup
-    REF 11e1a7459c37d9e52d5df8167d33aaec7ffa67dd
-    SHA512 9b597ebf9e08caf952c878b6e3a37e3412676110ac5dc9e9e6b55c302aed172b37994e78073d69aee8a3e1c42858ee9a2d1381a0609b5d190ddd6105b8a870d0
+    REF e0ddc34dcfef9cebb8a0ba03d3d6d8a1d8f2b2b8
+    SHA512 ae4c8ae115546f3dabd66622c509ad374936c69e8c656f881b7a79d585bd72b7c56a8d500cca3708bf8ab16089d56cde8f283949b1479913da34947c65d2f499
 )
 
 vcpkg_cmake_configure(
