c1f79dfda8225030d6d6333ea9ad697f0b530263
SineStriker <trueful@163.com>
2026-08-27 13:39:06 +0800

    Update stdcorelib plugin for catalog support

--- ports/stdcorelib-plugin/portfile.cmake
+++ ports/stdcorelib-plugin/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO stdware/stdcorelib.plugin
-    REF b1c992a94b39bffe9f78335c3723f4798b784269
-    SHA512 6abe9932d3229eeb3cb7cbf20d0448beef69647d3394219f89eda1404d16ed68f35ea8051c47b4f3df1de21a4cdf60037fb43a1dc2795d8e02347d61aee807fc
+    REF 200009a160bff9972114dbc8d822b17a65f52d5f
+    SHA512 d6fe25f620ce8f652b956fe621a4ac6463dea969e48749b711b00bbb1cd9500fa6579b3250399e3b74da75b0bdfe8fcfc0d1714c8d25e4b80f42f96e27b8d88f
 )
 
 vcpkg_cmake_configure(
